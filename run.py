d7023073ad396a42a0571b67288719b039635b89
qhduan <mail@qhduan.com>
2022-02-17 04:39:34 +0800

    update

--- run.py
+++ run.py
@@ -6,9 +6,10 @@ today = str(datetime.now())[:10]
 stock_zh_a_spot_em_df = ak.stock_zh_a_spot_em()
 # print(stock_zh_a_spot_em_df)
 if len(stock_zh_a_spot_em_df) > 100:
-    data = stock_zh_a_spot_em_df.to_json()
+    obj = json.loads(stock_zh_a_spot_em_df.to_json(orient='records'))
+    output = json.dumps(obj, indent=4, ensure_ascii=False)
     root = 'market_daily'
     os.makedirs(root, exist_ok=True)
-    output_path = os.path.join(root, today + 'json')
+    output_path = os.path.join(root, today + '.json')
     with open(output_path, 'w') as fp:
         fp.write(data)
